f2ec57501505dfdd8cfea2d5407e60236309ad8b
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-08-24 19:46:16 +0300

    Edit controoller and add 2 redirect method

--- ManageClient/Controllers/HomeController.cs
+++ ManageClient/Controllers/HomeController.cs
@@ -23,40 +23,34 @@ namespace WebApplication.Controllers
             _mongoDBService = context;
         }
 
-        public async Task<IActionResult> Index(Playlist playlist)
-        {
-            var phones = await _mongoDBService.GetProducts(playlist.name,playlist.surname);
-            //var model = new IndexViewModel { Products = phones, Filter = filter };
-            ViewBag.test = phones;
+        //public async Task<IActionResult> Index(Playlist playlist)
+        //{
+        //    var phones = await _mongoDBService.GetProducts(playlist.name,playlist.surname);
+        //    //var model = new IndexViewModel { Products = phones, Filter = filter };
+        //    ViewBag.test = phones;
 
+        //    ViewBag.hide_layout = "true";
+        //    return View();
+        //}
+
+        public IActionResult Index()
+        {
             ViewBag.hide_layout = "true";
             return View();
         }
 
-        //[HttpGet]
-        //public async Task<List<Playlist>> Get() {
-        //    return _mongoDBService.Get();
-        //}
+        [HttpPost]
+        public IActionResult Redirect_SignIn()
+        {
+            return RedirectToAction("SignIn","SignIn");
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Post([FromBody] Playlist playlist) { }
+        [HttpPost]
+        public IActionResult Redirect_SignUp()
+        {
+            return RedirectToAction("SignUp", "SignUp");
+        }
 
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> AddToPlaylist(string id, [FromBody] string movieId) { }
-
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> Delete(string i
-
-
-        //public IActionResult Privacy()
-        //{
-        //    return View();
-        //}
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
+        
     }
 }
