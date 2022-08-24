993a0b5ecf5bd2311afe7c539545c21250cd392e
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-08-24 19:46:38 +0300

    Added 2 new page

---
+++ ManageClient/Controllers/SignUpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageClient.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
