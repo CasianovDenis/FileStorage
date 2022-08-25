a92de44327d6bad49e93053c59c09b3540c21b21
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-08-25 18:25:38 +0300

    Added MSSQL connection and class for entity framework

---
+++ ManageClient/Models/MSSQL/UserServices.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageClient.Models
+{
+    public class UserServices
+    {
+
+
+        private readonly ConString _conString;
+
+        public UserServices(ConString conection)
+        {
+            _conString = conection;
+
+        }
+        //public UserServices(){ }
+
+
+
+        //public int user_id(string email)
+        //{
+
+        //    try
+        //    {
+        //        var id = conection.users.Single(data => data.Email == email);
+        //        return id.Id;
+        //    }
+        //    catch
+        //    {
+        //        return 0;
+        //    }
+
+        //}
+
+        //public IEnumerable<Users> GetAll()
+        //{
+        //    var getall = conection.users.Where(data => data.Id > 0).ToList();
+        //    return getall;
+        //}
+
+        public UserData Get(string username)
+        {
+            try
+            {
+                return _conString.UserData.AsNoTracking().Single(data => data.Username == username);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        //public Users Get(string email)
+        //{
+        //    try
+        //    {
+        //        return conection.users.AsNoTracking().Single(data => data.Email == email);
+        //    }
+        //    catch
+        //    {
+        //        return null;
+        //    }
+        //}
+
+
+        public void Create(UserData user)
+        {
+            _conString.UserData.Add(user);
+            _conString.SaveChanges();
+        }
+
+        public void Update(UserData user)
+        {
+            _conString.Update(user);
+            _conString.SaveChanges();
+        }
+
+        //public void Delete(int id)
+        //{
+        //    Users users = conection.users.Find(id);
+        //    if (users != null)
+        //        conection.users.Remove(users);
+
+        //    conection.SaveChanges();
+        //}
+
+        //public void Delete(string email)
+        //{
+        //    try
+        //    {
+        //        Users users = conection.users.Single(data => data.Email == email);
+        //        conection.users.Remove(users);
+        //        conection.SaveChanges();
+        //    }
+        //    catch
+        //    {
+
+        //    }
+
+        //}
+    }
+}
