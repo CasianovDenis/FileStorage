b812d24fcab8bee3c50c7c1cc9565a63a18913d4
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-08-25 18:25:10 +0300

    implemented backend logic for SignUp

--- ManageClient/Controllers/SignUpController.cs
+++ ManageClient/Controllers/SignUpController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using ManageClient.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,70 @@ namespace ManageClient.Controllers
 {
     public class SignUpController : Controller
     {
+        private readonly ConString _conString;
+
+        public SignUpController(ConString conection)
+        {
+            _conString = conection;
+
+        }
+
         public IActionResult SignUp()
         {
+            ViewBag.hide_layout = "true";
+            ViewBag.hide_footer = "true";
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserData user)
+        {
+
+            ValidationUserData validationRules = new ValidationUserData(user);
+
+            ValidationResult result = validationRules.Validate(user);
+
+
+
+            if (result.IsValid == false)
+            {
+                
+                result.AddToModelState(this.ModelState, "error posible");
+                SignUp();
+                return View();
+            }
+
+            //if (user.Username==""||user.Email==""||user.Password=="")
+            //{
+            //    ViewData["Error_SignUp"] = "";
+            //    SignUp();
+            //}
+            try
+            {
+                var dbdata = _conString.UserData.Single(data => data.Username == user.Username);
+                
+            }
+            catch
+            {
+                try
+                {
+                    var dbdata = _conString.UserData.Single(data => data.Email == user.Email);
+                }
+                catch
+                {
+                    UserServices userServices = new UserServices(_conString);
+                    userServices.Create(user);
+                    ViewData["Succes_create"] = "User was created succesfully";
+                    SignUp();
+                    return View();
+                }
+                
+            }
+
+            ViewData["Error_SignUp"] = "This user already exist";
+            SignUp();
+            return View();
+
         }
     }
 }
