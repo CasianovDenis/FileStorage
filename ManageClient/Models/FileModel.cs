9e89d7210c565ecfd6a7ee9069263c43ba5ff01b
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-09-01 23:47:39 +0300

    Create new class which add file in mongodb

---
+++ ManageClient/Models/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageClient.Models
+{
+    public class FileModel
+    {
+        public IFormFile file { get; set; }
+        public FileStream mydata { get; set; }
+    }
+}
