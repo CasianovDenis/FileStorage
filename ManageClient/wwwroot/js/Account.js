12038d4560135aa4d4e99ef2054723abfc8cbd7b
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-09-01 23:43:43 +0300

    Added style and scrypt for account view

---
+++ ManageClient/wwwroot/js/Account.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#file_btn').click(function () {
+        $('#uploadfile').click();
+
+        $("#uploadfile").on('change', function () {
+            $('#upload_input').click();
+        });
+        
+    });
+});
