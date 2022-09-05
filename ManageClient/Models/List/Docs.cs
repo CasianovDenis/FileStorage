7aad7b9a8fb5372e0c169ab1754aeecb334a00cb
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-09-05 12:13:44 +0300

    Added 2 new class for list

---
+++ ManageClient/Models/List/Docs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageClient.Models.List
+{
+    public class Docs
+    {
+        public string _id = null!;
+        public string type_doc = null!;
+        public string name_doc = null!;
+        public string base64 = null!;
+    }
+}
