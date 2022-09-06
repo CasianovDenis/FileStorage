8a883cb2de90e575071b9e2083a0667d700df732
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-09-06 20:35:55 +0300

    Added script for laoding animation page

--- ManageClient/wwwroot/js/Account.js
+++ ManageClient/wwwroot/js/Account.js
@@ -1,4 +1,33 @@
-﻿$(document).ready(function () {
+﻿var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function () {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
+
+
+
+$(document).ready(function () {
+
+    $("#nav-bar").show();
+    $("#header").show();
+
     var id_file;
     var url_file;
     var type_file;
